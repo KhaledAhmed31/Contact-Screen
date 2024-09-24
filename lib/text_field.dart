7bf056c0119ab50ff998d66c1ac1698431a4d86e
khaled Ahmed <khaledahm79@gmail.com>
2024-09-24 20:52:44 +0300

    edited text field

---
+++ lib/text_field.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatefulWidget {
+  const MyTextField({super.key, required this.hint, required this.controller});
+  final String hint;
+  final TextEditingController controller;
+
+  @override
+  State<MyTextField> createState() =>
+      // ignore: no_logic_in_create_state
+      TextFieldState(controller: controller, hint: hint);
+}
+
+class TextFieldState extends State<MyTextField> {
+  final String hint;
+  final TextEditingController controller;
+
+  TextFieldState({required this.hint, required this.controller});
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) return 'Field is empty';
+      },
+      cursorColor: Colors.blue,
+      controller: controller,
+      decoration: InputDecoration(
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.blue)),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(color: Colors.blue)),
+          hintText: hint,
+          suffixIcon: const Icon(
+            size: 23,
+            Icons.edit,
+            color: Colors.blue,
+          ),
+          fillColor: Colors.white,
+          filled: true,
+          border: const OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30)))),
+    );
+  }
+}
